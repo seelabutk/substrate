3d7cf20c37af309b04ab511b8ac6ddc5fe22058a
npatel79 <npatel79@vols.utk.edu>
2022-04-05 16:30:06 -0400

    fixes

--- src/substrate/substrate.py
+++ src/substrate/substrate.py
@@ -10,13 +10,13 @@ from aws_cdk.core import App
 import yaml
 
 from .classes import SubstrateStack, SubstrateSwarm
-from .tools import OSPRayStudio, Tapestry, VCI, DCHM
+from .tools import DCHM, OSPRayStudio, Tapestry, VCI
 
 MODULES = {
+	'dchm': DCHM,
 	'ospray_studio': OSPRayStudio,
 	'tapestry': Tapestry,
-	'vci': VCI,
-	'DCHM': DCHM 
+	'vci': VCI
 }
 
 
@@ -160,7 +160,7 @@ def main():
 
 	parser.add_argument(
 		'tool',
-		help='The visualization tool to run [ospray_studio, tapestry, vci, dchm]',
+		help='The visualization tool to run [dchm, ospray_studio, tapestry, vci]',
 		metavar='TOOL'
 	)
 	parser.add_argument(
